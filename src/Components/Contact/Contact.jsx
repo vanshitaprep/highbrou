bd9beb0a99d590fdbfeb8be2e2335762bd6028c4
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2025-02-02 18:55:14 +0530

    Updated Contact Images

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import "./Contact.css"
 import CommonTopBannerDynamic from "../CommonTopBanner/CommonTopBannerDynamic";
 import { Row, Col } from "antd";
+import Contactback from "./Contactback.jpg"
 const Contact = () => {
     useEffect(() => {
         window.scrollTo(0, 0);
@@ -12,7 +13,7 @@ const Contact = () => {
                 <CommonTopBannerDynamic
                     heading="Contact"
                     subheading="Connect Us"
-                    image="https://images.unsplash.com/photo-1705233844148-c554578cb64b?q=80&w=2874&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+                    image={Contactback}
                 />
                 <div>
                     <div className="sectionPadding"> 
